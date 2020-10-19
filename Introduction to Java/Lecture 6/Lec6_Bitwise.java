dfe4834cff9ad3dc759a05de0f3db431ed1b451d
Anmol Pant <anmolpant@gmail.com>
2020-10-19 23:57:37 +0530

    Lecture 6

---
+++ Introduction to Java/Lecture 6/Lec6_Bitwise.java
@@ -0,0 +1,17 @@
+
+public class Lec6_Bitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(2 & 3);
+		System.out.println(2 | 3);
+		System.out.println(2 ^ 3);
+		System.out.println(~2);
+		System.out.println(34 << 1);
+		System.out.println(34 >>2);
+		System.out.println(-4 << 1);
+		System.out.println(-4 >> 1);
+		
+	}
+
+}
