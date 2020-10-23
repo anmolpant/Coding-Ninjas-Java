82585deefe087e52536264e9e578c5f021f0a4d6
Anmol Pant <anmolpant@gmail.com>
2020-10-23 18:51:14 +0530

    completed lec7

---
+++ Introduction to Java/Lecture 7/Lec7_Increment.java
@@ -0,0 +1,17 @@
+
+public class Lec7_Increment {
+	
+	public static void increment(int n) {
+		n = n+1;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		increment(a);
+		System.out.println(a);
+
+	}
+
+}
