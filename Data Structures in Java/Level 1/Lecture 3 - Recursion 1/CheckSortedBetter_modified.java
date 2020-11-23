66210c9a02f92eb022f4053dadcd0629ae6b881c
Anmol Pant <anmolpant@gmail.com>
2020-11-23 21:40:29 +0530

    array recursion lecture codes

---
+++ Data Structures in Java/Level 1/Lecture 3 - Recursion 1/CheckSortedBetter_modified.java
@@ -0,0 +1,75 @@
+
+public class CheckSortedBetter_modified {
+		public static boolean checkSorted(int input[]){
+			
+			if(input.length <= 1){
+				return true;
+			}
+			
+			int smallInput[] = new int[input.length - 1];
+			for(int i = 1; i < input.length; i++){
+				smallInput[i - 1] = input[i];
+			}
+			
+			boolean smallAns = checkSorted(smallInput);
+			if(!smallAns){
+				return false;
+			}
+			if(input[0] <= input[1]){
+				return true;
+			}else{
+				return false;
+			}
+			
+		}
+	public static boolean checkSorted_2(int input[]){
+			
+			if(input.length <= 1){
+				return true;
+			}
+			if(input[0] > input[1]){
+				return false;
+			}
+			 
+			int smallInput[] = new int[input.length - 1];
+			for(int i = 1; i < input.length; i++){
+				smallInput[i - 1] = input[i];
+			}
+			boolean smallAns = checkSorted_2(smallInput);
+			return smallAns;
+//			if(smallAns){
+//				return true;
+//			}else{
+//				return false;
+//			}
+
+	}
+
+	// This function checks if the array is sorted from startIndex to end
+	private static boolean checkSortedBetter(int input[],int startIndex){
+		
+		if(startIndex >= input.length - 1){
+			return true;
+		}
+		if(input[startIndex] > input[startIndex + 1]){
+			return false;
+		}
+		boolean smallAns = checkSortedBetter(input, startIndex + 1);
+		return smallAns;
+		
+	}
+	
+	//function overloading
+	public static boolean checkSortedBetter(int input[]) {
+		return checkSortedBetter(input, 0);
+	}
+
+
+
+		public static void main(String[] args) {
+			int input[] = {1,2,3};
+			System.out.println(checkSortedBetter(input));
+
+		}
+
+	}
