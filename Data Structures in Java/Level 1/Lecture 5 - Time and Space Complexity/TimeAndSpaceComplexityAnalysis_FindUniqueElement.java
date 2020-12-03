9f185bd65fe67dc4f1e165535e7def5c43dc80b2
Anmol Pant <anmolpant@gmail.com>
2020-12-03 21:39:20 +0530

    find unique element

---
+++ Data Structures in Java/Level 1/Lecture 5 - Time and Space Complexity/TimeAndSpaceComplexityAnalysis_FindUniqueElement.java
@@ -0,0 +1,14 @@
+
+public class TimeAndSpaceComplexityAnalysis_FindUniqueElement {
+
+	public static int findUnique(int[] arr) {
+		//Your code goes here
+        
+        int ans = 0;
+        for(int i = 0; i<arr.length; i++){
+            ans = ans ^ arr[i];
+        }
+        
+        return ans;
+    }
+}
