98e3f8daa0e0c485b0239ca0a4bb77216f45ed66
Anmol Pant <anmolpant@gmail.com>
2020-12-09 23:58:07 +0530

    test1 codes

---
+++ Data Structures in Java/Level 1/Test 1/Test1_MaximumProfitOnApp.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Test1_MaximumProfitOnApp {
+
+		public static int maximumProfit(int budget[]) {
+			// Write your code here
+	        int ans = Integer.MIN_VALUE;
+	        int n = budget.length;
+	        
+	        //sort array
+	        Arrays.sort(budget);
+	        
+	        for(int i = 0; i<n; i++){
+	            
+	            ans = Math.max(ans, budget[i] * (n-i));
+	        }
+	        
+	        return ans;
+
+}
