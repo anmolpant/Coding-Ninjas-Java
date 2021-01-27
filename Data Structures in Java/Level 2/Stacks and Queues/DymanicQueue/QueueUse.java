6cc29245531d546e2ec05f4afb4da68c90c2951f
Anmol Pant <anmolpant@gmail.com>
2021-01-27 22:56:28 +0530

    dynamic queue

---
+++ Data Structures in Java/Level 2/Stacks and Queues/DymanicQueue/QueueUse.java
@@ -0,0 +1,27 @@
+
+public class QueueUse {
+
+	public static void main(String[] args) {
+		QueueUsingArray queue = new QueueUsingArray(3);
+		for(int i = 1; i <= 5; i++){
+			queue.enqueue(i);
+			try {
+				queue.enqueue(i);
+			} catch (QueueFullException e) {
+				
+			}
+		}
+		
+		
+		while(! queue.isEmpty()){
+			try {
+				System.out.println(queue.dequeue());
+			} catch (QueueEmptyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
