6df0e44283768e2f4f379b7e14e86c89cae29f9a
Anmol Pant <anmolpant@gmail.com>
2021-01-28 14:56:25 +0530

    queue w LL

---
+++ Data Structures in Java/Level 2/Stacks and Queues/Queue_Using_LL/QueueUse.java
@@ -0,0 +1,28 @@
+
+public class QueueUse {
+
+	public static void main(String[] args) {
+//		QueueUsingArray queue = new QueueUsingArray(3);
+		QueueUsingLL<Integer> queue = new QueueUsingLL<>();
+		for(int i = 1; i <= 5; i++){
+			queue.enqueue(i);
+//			try {
+//				queue.enqueue(i);
+//			} catch (QueueFullException e) {
+//				
+//			}
+		}
+		
+		
+		while(! queue.isEmpty()){
+			try {
+				System.out.println(queue.dequeue());
+			} catch (QueueEmptyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
