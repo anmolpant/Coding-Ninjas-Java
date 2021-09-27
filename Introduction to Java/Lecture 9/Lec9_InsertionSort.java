41bc8f332bfac12405dc3f67c6b9aef84eee0e9a
Abhishek Vishwakarma <73271173+dabbang123@users.noreply.github.com>
2021-09-27 15:18:23 +0530

    Update Lec9_InsertionSort.java

--- Introduction to Java/Lecture 9/Lec9_InsertionSort.java
+++ Introduction to Java/Lecture 9/Lec9_InsertionSort.java
@@ -1,4 +1,14 @@
 
 public class Lec9_InsertionSort {
+  for(int i = 1; i < arr.length; i++){
+			
+			int j = i - 1;
+			int temp = arr[i];
+			while(j>= 0 && arr[j] > temp){
+				arr[j + 1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = temp;
+		}
 
 }
